8ac9596ffa8202e110eaeafedc5a6671c80d7d7b
Zhao Peng <patchao2000@gmail.com>
2014-05-09 18:39:46 +0800

    1

--- test_maven/src/main/java/test_maven/Test.java
+++ test_maven/src/main/java/test_maven/Test.java
@@ -116,4 +116,66 @@ public class Test extends TestCase {
                 .processInstanceId(processId).singleResult();
         System.out.println("流程结束时间："+historicProcessInstance.getEndTime());
 	}
+
+	public void test2() {
+        ProcessEngine processEngine = ProcessEngineConfiguration
+                .createProcessEngineConfigurationFromResource("activiti.cfg.xml")
+                .buildProcessEngine();
+		// Create Activiti process engine
+//		ProcessEngine processEngine = ProcessEngineConfiguration
+//		    .createStandaloneProcessEngineConfiguration()
+//		    .buildProcessEngine();
+
+		// Get Activiti services
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+
+		// Deploy the process definition
+		repositoryService.createDeployment()
+			.addClasspathResource("FinancialReportProcess.bpmn")
+			.deploy();
+
+		// Start a process instance
+	    String procId = runtimeService.startProcessInstanceByKey("financialReport").getId();
+
+		// Get the first task
+	    TaskService taskService = processEngine.getTaskService();
+	    List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("accountancy").list();
+	    for (Task task : tasks) {
+	      System.out.println("Following task is available for accountancy group: " + task.getName());
+
+	      // claim it
+	      taskService.claim(task.getId(), "fozzie");
+	    }
+
+	    // Verify Fozzie can now retrieve the task
+	    tasks = taskService.createTaskQuery().taskAssignee("fozzie").list();
+	    for (Task task : tasks) {
+	      System.out.println("Task for fozzie: " + task.getName());
+
+	      // Complete the task
+	      taskService.complete(task.getId());
+	    }
+
+	    System.out.println("Number of tasks for fozzie: "
+	            + taskService.createTaskQuery().taskAssignee("fozzie").count());
+
+	    // Retrieve and claim the second task
+	    tasks = taskService.createTaskQuery().taskCandidateGroup("management").list();
+	    for (Task task : tasks) {
+	      System.out.println("Following task is available for accountancy group: " + task.getName());
+	      taskService.claim(task.getId(), "kermit");
+	    }
+
+	    // Completing the second task ends the process
+	    for (Task task : tasks) {
+	      taskService.complete(task.getId());
+	    }
+
+	    // verify that the process is actually finished
+	    HistoryService historyService = processEngine.getHistoryService();
+	    HistoricProcessInstance historicProcessInstance =
+	      historyService.createHistoricProcessInstanceQuery().processInstanceId(procId).singleResult();
+	    System.out.println("Process instance end time: " + historicProcessInstance.getEndTime());
+	}
 }
